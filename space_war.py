b039ad9568639aeffee124a39f1023138ecae8ff
Rob Prouse <rprouse@reliqhealth.com>
2019-04-29 20:34:40 -0400

    Background color

--- space_war.py
+++ space_war.py
@@ -1,20 +1,27 @@
 import sys
 import pygame
+# from pygame.locals import *
 
-def run_game():
+def main():
     # Initialize the game
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Space War")
+
+    bg_color = (10, 30, 60)
 
     # Main game loop
     while(True):
         # Keyboard and mouse input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+                return
+
+        screen.fill(bg_color)
 
         # Draw the screen
         pygame.display.flip()
 
-run_game()
+#call the "main" function if running this script
+if __name__ == '__main__':
+    main()
